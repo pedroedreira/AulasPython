87b91c84f6e2cd6eb3494448ea07747aa92c787f
Pedro Edreira <pedroviniciusedreira@gmail.com>
2025-06-04 17:37:19 -0300

    aula de calculos basicos

---
+++ aula-calculos.py
@@ -0,0 +1,10 @@
+nomeComprador = "Pedro Vinícius"
+produto = "Notebook"
+precoNotebook = 2500
+qtd = 2 
+
+print("Olá,",nomeComprador)
+print("Produto:",produto)
+print(qtd)
+print("Preço da Unidade:", precoNotebook)
+print("Total pago: R$", qtd*precoNotebook)
