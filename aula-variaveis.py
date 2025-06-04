5eba6260daabf0f56f9b41745a1b1b6db832108d
Pedro Edreira <pedroviniciusedreira@gmail.com>
2025-06-04 17:28:57 -0300

    aula de variáveis

---
+++ aula-variaveis.py
@@ -0,0 +1,14 @@
+#str - string (textos)
+#int - números inteiros
+#float - números reais 
+#bool - lógico (verdadeiro ou falso)
+
+nome = "Pedro Edreira"
+idadeUsuario = 25
+profissaoUsuario = "Analista de Sistemas"
+atualSalario = 2300.50
+
+print("Nome do Usuário:", nome) 
+print("Idade do Usuário:", idadeUsuario) 
+print("Profissão do Usuário:", profissaoUsuario) 
+print("Salário atual do Usuário:", atualSalario) 
