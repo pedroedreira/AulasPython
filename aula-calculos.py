dc97960cf06f1f104645f9856ea4622b8562e4fc
Pedro Edreira <pedroviniciusedreira@gmail.com>
2025-06-05 12:09:25 -0300

    att aula calculos + inicio aula dizimas

--- aula-calculos.py
+++ aula-calculos.py
@@ -2,9 +2,10 @@ nomeComprador = "Pedro Vinícius"
 produto = "Notebook"
 precoNotebook = 2500
 qtd = 2 
+total = qtd*precoNotebook
 
 print("Olá,",nomeComprador)
 print("Produto:",produto)
 print(qtd)
 print("Preço da Unidade:", precoNotebook)
-print("Total pago: R$", qtd*precoNotebook)
+print("Total pago: R$",total)
