b2632fdf06ade3b345b9006fa49a4544a340cb4c
Pedro Edreira <pedroviniciusedreira@gmail.com>
2025-06-06 11:38:21 -0300

    exemplo pratico pdv

--- exemplo-pratico.py
+++ exemplo-pratico.py
@@ -3,5 +3,15 @@ preco = 203.98
 qtd = input("Digite a quantidade de produtos desejada:")
 qtd = int(qtd)
 resultado = qtd*preco
-nomeCliente = 
-doctoCliente = 
+nomeCliente = input("Digite seu nome completo:")
+doctoCliente = input("Insira seu ID:")
+
+print("------RELATÓRIO------")
+print("---INFORMAÇÕES DO COMPRADOR---")
+print("NOME DE USUÁRIO:",nomeCliente)
+print("ID DO USUÁRIO:",doctoCliente)
+print("---INFORMAÇÕES DA COMPRA---")
+print("PRODUTO ADQUIRIDO:",produto)
+print("PREÇO UNITÁRIO: R$",preco)
+print("QUANTIDADE ADQUIRIDA:",qtd)
+print(f"TOTAL PARA PAGAMENTO: R${resultado:.2f}")
